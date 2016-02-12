2e5fe690a544a37f9626903c79a1a0243f2e5142
Justin Fargione <justin@lexicalbits.com>
2016-02-12 10:57:34 -0500

    First working version

--- fixtures/default.js
+++ fixtures/default.js
@@ -3,17 +3,7 @@ var _ = require('lodash')
     ;
 
 module.exports = _.merge({
-    /*
-     * Some default settings. 
-     *
-     * You can generally leave this as is for general testing purposes.
-     */
-    simulation: true
-    , instance_id: 'local_test_instance'
-    , urls: {
-        home: "http://rundexter.com/"
-    }
-    , instance_state: {
+    instance_state: {
         active_step :  "local_test_step"
     }
     , workflow: {
@@ -33,47 +23,13 @@ module.exports = _.merge({
     , modules: {
         //The test runner will add the proper data here
     }
-    /*
-     * End defaults
-     */
-    , environment: {
-       /*
-        * Any API keys you might need should go in the env.js.
-        * For example:
-        *
-        "parse_app_id": "abc123"
-        , "parse_app_key": "foobar"
-        */
-    }
-    , user: {
-        /*
-         * Your dexter user settings should go in the env.js file and remain uncommitted.  
-         * For example:
-         *
-        profile: {
-            id: 1,
-            api_key: 'apikeytest'
-        }
-         */
-        /*
-         * You should also add your providers to env.js
-         * Example:
-        providers: {
-            github: {
-                access_token: 'abc123',
-                username: 'foo'
-            }
-        }
-         */
-    }
     , data: {
         local_test_step: {
             /*
              * You should update this section with some test input for testing your module
              */
             input: {
-                //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                stations: ['8468191', '8467150']
             }
         }
     }
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var _ = require('lodash')
+    , request = require('request')
+    , qs = require('qs')
+    , Q = require('q')
+    , BASE_URL = 'http://tidesandcurrents.noaa.gov/api/datagetter'
+;
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +12,61 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var stations = step.input('stations').toArray()
+            , self = this
+            , baseQuery = {
+                date: 'latest'
+                , product: 'wind'
+                , units: 'english'
+                , time_zone: 'gmt'
+                , format: 'json'
+            }
+        ;
+        Q.allSettled(_.map(stations, function(station) {
+            var query = qs.stringify(_.extend(baseQuery, { station: station }));
+            return self.query(query);
+        }))
+            .then(function(allResponses) {
+                self.complete(_.compact(_.map(allResponses, function(resp) {
+                    var data;
+                    if(resp.state !== 'fulfilled') {
+                        self.log('Invalid response from server', { response: resp });
+                        return null;
+                    }
+                    try {
+                        data = JSON.parse(resp.value.body);
+                    } catch(e) {
+                        self.log('Invalid JSON data from server', { data: resp.value.body });
+                        return null;
+                    }
+                    if(data.error) {
+                        self.log('Error response', { error: data.error });
+                        return null;
+                    }
+                    return ({
+                        time: _.get(data, 'data[0].t', null)
+                        , speed: _.get(data, 'data[0].s', null)
+                        , direction: _.get(data, 'data[0].dr', null)
+                        , gust: _.get(data, 'data[0].g', null)
+                        , station_id: _.get(data, 'metadata.id', null)
+                        , station_name: _.get(data, 'metadata.name', null)
+                    });
+                })));
+            })
+            .fail(function(err) {
+                self.fail(err);
+            })
+        ;
     }
+    , query: function(query) {
+        var url = BASE_URL + '?' + query
+            , deferred = Q.defer()
+        ;
+        request.get(url, function(err, resp) {
+            if(err) deferred.reject(err);
+            else deferred.resolve(resp); 
+        });
+        return deferred.promise;
+    }
+
 };
